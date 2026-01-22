00b1556bad9897287ba62f2475810d3ec33f24bd
pro0grammer <abbasnadir12345@gmail.com>
2026-01-22 23:35:42 +0530

    Added patch method to profile route

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -11,8 +11,8 @@ const profileRouter: RouterObject = {
       method: "get",
       props: "/",
       authorization: "required",
-      rateLimit: "strict",
-      keyType: "default",
+      rateLimit: "read",
+      keyType: "user",
       handler: async (req: Request, res: Response) => {
         const { data: profile, error } = await supabase
           .from("profiles")
@@ -23,6 +23,50 @@ const profileRouter: RouterObject = {
 
         if (error || !profile) {
           throw new NotFoundError(error.message || "User not found");
+        }
+
+        const user = {
+          id: req.user.id,
+          email: req.user.email,
+          ...profile,
+        };
+
+        res.status(200).json(user);
+      },
+    },
+    {
+      method: "patch",
+      props: "/",
+      authorization: "required",
+      rateLimit: "strict",
+      keyType: "user",
+      handler: async (req: Request, res: Response) => {
+        const updates = {
+          username: req.body.username,
+          pfp: req.body.pfp,
+          status: req.body.status,
+        };
+
+        Object.keys(updates).forEach(
+          (k) =>
+            updates[k as keyof typeof updates] === undefined &&
+            delete updates[k as keyof typeof updates],
+        );
+
+        if (Object.keys(updates).length === 0) {
+          throw new UnauthorizedError("No fields to update");
+        }
+
+        const { data: profile, error } = await supabase
+          .from("profiles")
+          .update(updates)
+          .eq("id", req.user.id)
+          .is("deleted_at", null)
+          .select("username, pfp, status, created_at")
+          .single();
+
+        if (error || !profile) {
+          throw new NotFoundError(error?.message || "User not found");
         }
 
         const user = {
